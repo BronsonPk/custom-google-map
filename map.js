2be7ff9541849133e329b5c170908e58e693d4d9
BronsonPk <164549821+BronsonPk@users.noreply.github.com>
2024-03-25 07:57:30 -0600

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,14 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 48.8650289582911, lng: 2.3221745766147484 },
+        zoom: 15,
+    });
 
+    // Load the KML file
+    const kmlLayer = new google.maps.KmlLayer({
+        url: "path/to/your/kml/file.kml",
+        map: map,
+    });
+}
+
+window.initMap = initMap;
